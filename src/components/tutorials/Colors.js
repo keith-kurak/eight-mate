33c174c60a1de637d043dfb19c7183d952b2ad17
Keith Kurak <keith.kurak@gmail.com>
2022-10-18 00:47:59 -0400

    add correct colors

--- src/components/tutorials/Colors.js
+++ src/components/tutorials/Colors.js
@@ -2,28 +2,28 @@ import { View, Text } from "react-native";
 
 const colorRows = [
   [
-    { index: 0, color: "red" },
-    { index: 1, color: "orange" },
-    { index: 2, color: "yellow" },
-    { index: 3, color: "green" },
+    { index: 0, color: "#000000" },
+    { index: 1, color: "#1D2B53" },
+    { index: 2, color: "#7E2553" },
+    { index: 3, color: "#008751" },
   ],
   [
-    { index: 4, color: "red" },
-    { index: 5, color: "orange" },
-    { index: 6, color: "yellow" },
-    { index: 7, color: "green" },
+    { index: 4, color: "#AB5236" },
+    { index: 5, color: "#5F574F" },
+    { index: 6, color: "#C2C3C7" },
+    { index: 7, color: "#FFF1E8" },
   ],
   [
-    { index: 8, color: "red" },
-    { index: 9, color: "orange" },
-    { index: 10, color: "yellow" },
-    { index: 11, color: "green" },
+    { index: 8, color: "#FF004D" },
+    { index: 9, color: "#FFA300" },
+    { index: 10, color: "#FFEC27" },
+    { index: 11, color: "#00E436" },
   ],
   [
-    { index: 12, color: "red" },
-    { index: 13, color: "orange" },
-    { index: 14, color: "yellow" },
-    { index: 15, color: "green" },
+    { index: 12, color: "#29ADFF" },
+    { index: 13, color: "#83769C" },
+    { index: 14, color: "#FF77A8" },
+    { index: 15, color: "#FFCCAA" },
   ],
 ];
 
@@ -31,14 +31,14 @@ function ColorBox({ color }) {
   return (
     <View
       style={{
-        width: 50,
-        height: 50,
+        width: 70,
+        height: 70,
         backgroundColor: color.color,
         justifyContent: "center",
         alignItems: "center",
       }}
     >
-      <Text style={{ color: "white" }}>{color.index}</Text>
+      <Text style={{ color: color.index === 7 ? "black" : "white", fontSize: 20 }}>{color.index}</Text>
     </View>
   );
 }
